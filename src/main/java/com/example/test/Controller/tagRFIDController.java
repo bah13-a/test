338c7971486022622d9035b030bcc44bf463bafb
@w.baha <w.baha@waycon.local>
2022-11-10 09:02:17 +0100

    2eme commit

--- src/main/java/com/example/test/Controller/tagRFIDController.java
+++ src/main/java/com/example/test/Controller/tagRFIDController.java
@@ -3,23 +3,31 @@ package com.example.test.Controller;
 import com.example.test.Model.tagread;
 import com.example.test.Service.TagRFIDService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Path;
 
 @RestController
 public class tagRFIDController {
     @Autowired
     public TagRFIDService tagService;
+
     @GetMapping("/map")
     public tagread getTagRfid() throws IOException {
-       return tagService.TagMapper();
+        return tagService.TagMapper();
     }
 
     @PostMapping("/injecter")
     public tagread saveTag() throws FileNotFoundException {
         return tagService.SaveTag();
     }
+
+
+    @PostMapping("importer")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file ) throws InterruptedException {
+       return tagService.importer(file);
+}
 }
--- src/main/java/com/example/test/Service/TagRFIDService.java
+++ src/main/java/com/example/test/Service/TagRFIDService.java
@@ -2,41 +2,62 @@ package com.example.test.Service;
 
 import com.example.test.Model.tagread;
 import com.example.test.Repository.TagRFIDRepository;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.google.gson.Gson;
-import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class TagRFIDService {
     @Autowired
     private TagRFIDRepository tagRepo;
+    private String path ;
 
 
 
     public tagread TagMapper() throws FileNotFoundException {
         Gson gson=new Gson();
-        tagread tag=gson.fromJson("{\n" +
-                "  \"EPC\": \"E98A144587788652562502145639\",\n" +
-                "  \"Antenna\": 3,\n" +
-                "  \"Timestamp\": 1234567898765,\n" +
-                "  \"RSSI\": -5\n" +
-                "}",tagread.class);
 
-        if(!((tag.getEPC().length()==24)&&(Integer.toHexString(Integer.parseInt(tag.getEPC())).length()==0)&&(tag.getAntenna()>0)&&(tag.getAntenna()<5)&&((String.valueOf(tag.getTimestamp())).length()==13)&&((tag.getRSSI()<0)&&(tag.getRSSI()>-70))))
+        InputStream is = getClass().getResourceAsStream("/RFID.json");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        tagread tag=gson.fromJson(reader,tagread.class);
+
+        if(!this.valide(tag))
         {
-
             System.out.println("ERREUR");
         }
         return tag;
+    }
+    public boolean valide(tagread tag)
+    {
+        if(!((tag.getEPC().length()==24)&&(Integer.toHexString(Integer.parseInt(tag.getEPC())).length()==0)&&(tag.getAntenna()>0)&&(tag.getAntenna()<5)&&((String.valueOf(tag.getTimestamp())).length()==13)&&((tag.getRSSI()<0)&&(tag.getRSSI()>-70))))
+        {return true;}
+        else
+            return false;
     }
     public tagread SaveTag() throws FileNotFoundException {
         tagread tag=this.TagMapper();
         tagRepo.saveAndFlush(tag);
         return  tagRepo.save(tag);
     }
+   public String importer(MultipartFile file ) {
+
+        String fileName = file.getOriginalFilename();
+        try {
+            file.transferTo( new File("C:\\Users\\user\\Desktop\\timestampappli\\Test\\src\\main\\resources\\" +"RFID.json"));
+        } catch (Exception e) {
+            path=file.getOriginalFilename();
+            return path ;//ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+       path=file.getOriginalFilename();
+        return path+"importé" ;//ResponseEntity.ok("File uploaded successfully.");
+    }
+
+
+
+
 }
